dace3d3294406c5bb34f79af865651a74e381a18
Sergey Abadzhev <q3er@mail.ru>
2026-03-31 11:18:29 +0300

    Initial Commit

---
+++ WeatherAppUIKitTests/WeatherAppUIKitTests.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherAppUIKitTests.swift
+//  WeatherAppUIKitTests
+//
+//  Created by Sergey Abadzhev on 31.03.26.
+//
+
+import Testing
+@testable import WeatherAppUIKit
+
+struct WeatherAppUIKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
