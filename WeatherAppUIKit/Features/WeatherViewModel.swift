1268c4ef908ff380e3242a858e850a1d44450573
Sergey Abadzhev <q3er@mail.ru>
2026-04-01 14:21:24 +0300

    WeatherViewState

--- WeatherAppUIKit/Features/WeatherViewModel.swift
+++ WeatherAppUIKit/Features/WeatherViewModel.swift
@@ -8,43 +8,6 @@
 import Foundation
 import CoreLocation
 
-struct CurrentWeatherDisplay {
-    let cityName: String
-    let temperature: String
-    let conditionText: String
-    let hiLoText: String
-    let feelsLike: String
-    let humidity: String
-    let wind: String
-}
-
-struct HourlyItemDisplay {
-    let time: String
-    let conditionCode: Int
-    let temperature: String
-}
-
-struct DailyItemDisplay {
-    let dayName: String
-    let conditionCode: Int
-    let low: Double
-    let high: Double
-    let lowText: String
-    let highText: String
-    let globalLow: Double
-    let globalHigh: Double
-}
-
-enum WeatherViewState {
-    case loading
-    case loaded(
-        current: CurrentWeatherDisplay,
-        hourly: [HourlyItemDisplay],
-        daily: [DailyItemDisplay]
-    )
-    case error(String)
-}
-
 private struct TimeoutError: Error {}
 
 @MainActor
